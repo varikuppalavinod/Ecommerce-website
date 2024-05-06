dbb1d899aa83166c7532fd18584accdd9fdf0fa3
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-05-06 14:44:35 +0530

    api calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+//using async await
+import React,{useState} from 'react';
+
+import MovieList from './Components/MovieList';
 import './App.css';
 
 function App() {
+  const [Movies,setMovies]=useState([])
+ async function fetchmoviehandler(){
+ const response=await fetch("https://swapi.dev/api/films/")
+    const data= await response.json();
+
+      const transformedmovies=data.results.map(movieData=>{
+        return{
+        id:movieData.episode_id,
+        title:movieData.title,
+        openingText:movieData.opening_crawl,
+        releaseDate:movieData.release_date,
+      }
+      })
+     setMovies(transformedmovies)
+ 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchmoviehandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MovieList movies={Movies} />
+      </section>
+    </React.Fragment>
   );
 }
 
 export default App;
+
+
+
+//using promise .then
+/*
+import React,{useState} from 'react';
+
+import MovieList from './Components/MovieList';
+import './App.css';
+
+function App() {
+  const [Movies,setMovies]=useState([])
+  function fetchmoviehandler(){
+    fetch("https://swapi.dev/api/films/").then(response=>{
+      return response.json();
+    }).then(data=> {
+      const transformedmovies=data.results.map(movieData=>{
+        return{
+        id:movieData.episode_id,
+        title:movieData.title,
+        openingText:movieData.opening_crawl,
+        releaseDate:movieData.release_date,
+      }
+      })
+     setMovies(transformedmovies)
+    })
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchmoviehandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MovieList movies={Movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+
+/*
+const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+  */
