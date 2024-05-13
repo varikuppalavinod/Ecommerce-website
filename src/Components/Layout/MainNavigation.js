a8abcf4cfb9890238f42f3a343613367bd762926
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-05-13 14:40:01 +0530

    auto logout

--- src/Components/Layout/MainNavigation.js
+++ src/Components/Layout/MainNavigation.js
@@ -11,6 +11,7 @@ const MainNavigation = () => {
 
   const logouthandler=()=>{
     authctx.logout()
+    
     //redirect the user to login page
   }
   return (
--- src/Components/store/Authcontext.js
+++ src/Components/store/Authcontext.js
@@ -1,5 +1,5 @@
 
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 
 const Authcontext=React.createContext({
     token:"",
@@ -24,6 +24,24 @@ export const AuthContextProvider=(props)=>{
     setToken(null)
     localStorage.removeItem("token")
    }
+
+
+   //auto logout
+
+   
+  // Auto logout after 5 minutes of inactivity
+  useEffect(() => {
+    const logoutTimer = setTimeout(() => {
+      logoutHandler();
+      alert("please login again")
+    }, 1 * 60 * 1000); // 5 minutes in milliseconds
+    
+
+    return () => {
+      clearTimeout(logoutTimer);
+    };
+  }, [token]); // Reset timer whenever token changes
+
 
    const contextvalue={
      token:token,
